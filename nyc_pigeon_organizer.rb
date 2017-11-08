93ca8d209ef59c3bb9c97f9a86f1d7a01a43c928
devinencalada <devinencalada@gmail.com>
2017-11-07 19:52:37 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names = data[:gender][:male] + data[:gender][:female]
+  pigeon_hash = {}
+  pigeon_names.each do |name|
+    pigeon_hash[name] = {}
+    data.each do |property, values|
+      pigeon_hash[name][property] = []
+      values.each do |value, array|
+        if array.include?(name)
+          pigeon_hash[name][property] << value.to_s
+        end
+      end
+    end
+  end
+  return pigeon_hash
 end
